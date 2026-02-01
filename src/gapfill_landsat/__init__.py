736fefea62a4093f254ee1106bfa133746cf1823
Philip Blankenau <phil.blankenau@gmail.com>
2026-02-01 01:08:08 -0700

    Update email.

--- src/gapfill_landsat/__init__.py
+++ src/gapfill_landsat/__init__.py
@@ -1,7 +1,7 @@
 """Top-level package for gapfill-landsat."""
 
 __author__ = """Philip Blankenau"""
-__email__ = 'philip.blankenau@idwr.idaho.gov'
+__email__ = 'phil.blankenau@gmail.com'
 __version__ = '0.1.0'
 
 from .gapfill_landsat import nspi
